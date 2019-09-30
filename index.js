648d9dd3ef6e48727579aa8afbbc388907b50d1c
Fayan1103 <enida.vezelaj@hotmail.it>
2019-09-30 15:57:23 -0400

    better worded code

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-let boys = {
+let firstHalf = {
   'josh': ['sara', 'martha', 'chiara', 'lara', 'sofia'],
   'simon': ['chiara', 'lara', 'sofia', 'martha', 'sara'],
   'michael': ['sofia','sara', 'martha', 'lara', 'chiara'],
   'pete': ['lara', 'chiara', 'martha', 'lara', 'sofia'],
-
 }
 
-let ladies = {
+let secondHalf = {
   'sara': ['pete', 'josh', 'michael', 'simon'],
   'lara': ['michael', 'pete', 'josh', 'simon'],
   'martha': ['josh', 'simon', 'pete', 'michael'],
@@ -15,31 +14,22 @@ let ladies = {
 }
 
 
-let singleMen = []
-let singleLadies = []
+let firstFreeHalf = []
+let secondFreeHalf = []
 let matches = []//arr of arrs///[josh, sara],[simon, martha]
 
 
-const initMen = (boysObject) => {
-  for (boy in boysObject) {
-    singleMen.push(boy)
-  }
-}
-
-const initWomen = (ladiesObject) => {
-  for (lady in ladiesObject) {
-    singleLadies.push(lady)
+const init = (personObject, object) => {
+  for (person in personObject) {
+    object.push(person)
   }
 }
 
 
-
-
-
-const findCoupleIndex = (lady) => {
+const findPairIndex = (secondPerson) => {
   for (let i = 0; i < matches.length; i++) {
     let couple = matches[i];
-    if (couple[1] === lady) {
+    if (couple[1] === secondPerson) {
       return i
     }
   }
@@ -51,60 +41,60 @@ const remove = (person, array) => {
 }
 
 const addLast = () => {
-
-   let personLeft = singleLadies[0];
-   let guy = ladies[personLeft][0];
+   let personLeft = secondFreeHalf[0];
+   let firstLeft = secondHalf[personLeft][0];
    //guy.indexOf(person)
    for (let i = 0; i < matches.length; i++) {
     let couple = matches[i];
-    if (couple[0] === guy) {
+    if (couple[0] === firstLeft) {
       matches[i].push(personLeft)
     }
    }
 }
 
 
-const couple = (boy) => {
-  let boysChoises = boys[boy]
-
+const couple = (person) => {
+  let firstHalfPreferences = firstHalf[person]
   let i = 0;
-  while(i < boysChoises.length) {
-    let lady = boysChoises[i]
-    if (singleLadies.indexOf(lady) > -1) {//if she is single
-      matches.push([boy, lady])
+  while(i < firstHalfPreferences.length) {
+    let secondPerson = firstHalfPreferences[i]
+    if (secondFreeHalf.indexOf(secondPerson) > -1) {//if prson from second group is free
+      matches.push([person, secondPerson])
 
-      remove(boy, singleMen)//take dude out of single bucket
-      remove(lady, singleLadies)//take out lady out of bucket
+      remove(person, firstFreeHalf)//take dude out of single bucket
+      remove(secondPerson, secondFreeHalf)//take out lady out of bucket
       return;
     } else {//if she is not single
-      let index = findCoupleIndex(lady);
-      let currentMan = matches[index][0]
-      if (ladies[lady].indexOf(boy) < ladies[lady].indexOf(currentMan)) { //if this boy is higher on the list then her current man
-        matches[index][0] = boy;
-        remove(boy, singleMen);
-        singleMen.push(currentMan)
+      let index = findPairIndex(secondPerson);
+      let currentPerson = matches[index][0]
+      if (secondHalf[secondPerson].indexOf(person) < secondHalf[secondPerson].indexOf(currentPerston)) { //if this boy is higher on the list then her current man
+        matches[index][0] = person;
+        remove(person, firstFreeHalf);
+        firstFreeHalf.push(currentPerson)
       }
     }
     i++
   }
-
 }
 
+const matching = (first, second) => {
+  init(first, firstFreeHalf);//working
+  init(second, secondFreeHalf)
 
-const matching = (boys, ladies) => {
-  initMen(boys);//working
-  initWomen(ladies)
-
-  while (singleMen.length > 0) { //while there are still men available
-    singleMen.forEach((man) => {
-      couple(man)
+  while (firstFreeHalf.length > 0) { //while there are still men available
+    firstFreeHalf.forEach((person) => {
+      couple(person)
     })
   }
 
-  if (singleLadies.length > 0) {
+  if (secondFreeHalf.length > 0) {
     addLast()
   }
     return matches;
 }
 
-matching(boys, ladies)
+
+module.exports = {
+  matching,
+  remove
+}
